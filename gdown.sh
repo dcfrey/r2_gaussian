35dca428c2f06ea3df1aa6a69c6b37f7322bd3ad
Frey <ga36dem@kickseed.e17.ph.tum.de>
2025-01-18 08:01:18 +0100

    Add data download script

---
+++ gdown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+### real data
+
+# projections
+gdown https://drive.google.com/uc?id=1cgYzvxkrlN0u5F9Auiy6oBEqyqf3Wf69
+
+# 75 angle
+gdown https://drive.google.com/uc?id=12_IA9jMzK7HjuhQSoNX2Uf5HZIfqOtpA
